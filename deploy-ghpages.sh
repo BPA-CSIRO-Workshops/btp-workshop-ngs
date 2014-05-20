22e00bea456bf506046bc59a776d4c6f44e0fb8b
Nathan S. Watson-Haigh <nathan@watsonhaigh.net>
2014-05-20 23:05:13 +0930

    Update deploy-ghpages.sh
    [no ci]

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -3,7 +3,7 @@
 cd $HOME
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
- clone --quiet --branch=gh-pages https://${GH_TOKEN}@${GH_REF} gh-pages > /dev/null
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@${GH_REF} gh-pages > /dev/null
 
 # Commit and Push the Changes
 cd gh-pages
