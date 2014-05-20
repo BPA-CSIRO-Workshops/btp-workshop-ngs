7defaca2082a7b04c15cce8940c56328854578ce
Nathan S. Watson-Haigh <nathan@watsonhaigh.net>
2014-05-20 23:13:58 +0930

    Update deploy-ghpages.sh

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-# Get to the Travis build directory, configure git and clone the repo
-cd $HOME
+# Configure git and clone the repo
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
 git clone --quiet --branch=gh-pages https://${GH_TOKEN}@${GH_REF} gh-pages > /dev/null
 
 # Commit and Push the Changes
 cd gh-pages
-git rm -f ./pdfs-latest/*.pdf
-cp -Rf $HOME/traine{e,r}_*.pdf ./pdfs-latest
+cp -Rf ../traine{e,r}_*.pdf ./
 git add -f .
 git commit -m "Lastest PDFs on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
 git push -fq origin gh-pages > /dev/null
